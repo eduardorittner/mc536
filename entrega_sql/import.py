866f0cf9a42a8fdc47b5c5a17ce62b45ebf407d7
eduardorittner <eduardorittnerc@gmail.com>
2025-04-26 13:31:05 -0300

    more stuff

--- entrega_sql/import.py
+++ entrega_sql/import.py
@@ -1,24 +1,37 @@
 import psycopg2
+import pandas as pd
 
-sql_file = "./teste_db.sql"
+def create_tables(conn):
+    sql_file = "./teste_db.sql"
 
-with open(sql_file, "r") as f:
-    sql_script = f.read()
+    with open(sql_file, "r") as f:
+        sql_script = f.read()
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(sql_script)
+            conn.commit()
+
+        except Exception as e:
+            conn.rollback()
+            print(f"Erro: {e}")
+            exit(1)
+
+def import_csv(file):
+    # TODO read csv data and change columns names to match db
+
+    df = pd.read_csv(file)
+
+
+def insert_into_db(frame):
+    # Get a pandas frame and insert data into db
 
 with psycopg2.connect(
     dbname="Educacao-e-energia",
     user="postgres",
     password="",
     host="localhost",
-    port="5432",
-) as conn:
-    cursor = conn.cursor()
+    port="5432",) as conn:
 
-    try:
-        cursor.execute(sql_script)
-        conn.commit()
+    create_tables(conn)
 
-    except Exception as e:
-        conn.rollback()
-        print(f"Erro: {e}")
-        exit(1)
